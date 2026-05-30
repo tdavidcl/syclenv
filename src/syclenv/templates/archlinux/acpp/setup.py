5a9eadc245d5aa648e380000ff086cd383a7aece
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-05-30 20:30:36 +0200

    cleaner ?

--- src/syclenv/templates/archlinux/acpp/setup.py
+++ src/syclenv/templates/archlinux/acpp/setup.py
@@ -77,7 +77,7 @@ default_install_packages = [
 
 def install_prerequisites():
     run_cmd(
-        f"sudo pacman -S --noconfirm {' '.join(default_install_packages)}",
+        f"sudo pacman -Sy --noconfirm {' '.join(default_install_packages)}",
         log_cmd=True,
         log_error=True,
         live_output=True,
