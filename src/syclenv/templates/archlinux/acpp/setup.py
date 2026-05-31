f444fbf0e8ba76d2f117c9c43d19283e7d90f691
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-05-31 11:34:54 +0200

    more coherent structure

--- src/syclenv/templates/archlinux/acpp/setup.py
+++ src/syclenv/templates/archlinux/acpp/setup.py
@@ -108,7 +108,7 @@ class ArchLinuxAcppTemplate(TemplateBase):
         Path(self.args.path).mkdir(parents=True, exist_ok=True)
 
         template = ""
-        
+
         # add env vars to template
         for var, value in ENV_VARS.items():
             line = f"export {var}={value}"
@@ -124,7 +124,7 @@ class ArchLinuxAcppTemplate(TemplateBase):
 
         with open(self.args.path + "/activate.zsh", "w") as f:
             f.write(template)
-            
+
         template += "function _internal_deactivate {"
         for var in ENV_VARS.keys():
             template += f"  unset {var}\n"
