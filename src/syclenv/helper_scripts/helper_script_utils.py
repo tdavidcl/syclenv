00c6e2397dd607c98554a711406f4d60c0ccdbf9
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-06-01 21:33:55 +0200

    yolo

--- src/syclenv/helper_scripts/helper_script_utils.py
+++ src/syclenv/helper_scripts/helper_script_utils.py
@@ -4,7 +4,7 @@ SPACER = "\n####################################################################
 def fetch_helper_script(path: str) -> str:
     with open(path) as f:
         helper_script = ""
-        helper_script += f"{SPACER}\n# Imported script " + f + f"{SPACER}\n"
+        helper_script += f"{SPACER}\n# Imported script " + path + f"{SPACER}\n"
         helper_script += f.read()
         helper_script += f"{SPACER}{SPACER}{SPACER}\n"
 
