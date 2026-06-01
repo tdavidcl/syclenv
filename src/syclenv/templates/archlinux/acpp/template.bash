7b78995501b3c6ba6ab7c17109af684f306440c1
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-06-01 22:20:11 +0200

    yolo

--- src/syclenv/templates/archlinux/acpp/template.bash
+++ src/syclenv/templates/archlinux/acpp/template.bash
@@ -1,28 +1,3 @@
-
-function clone_acpp {
-
-    ACPP_URL="https://github.com/AdaptiveCpp/AdaptiveCpp.git"
-
-    if [ -z ${ACPP_GIT_DIR+x} ]; then echo "ACPP_GIT_DIR is unset"; return 1; fi
-
-    if [ ! -f "$ACPP_GIT_DIR/README.md" ]; then
-        echo " ------ Clonning AdaptiveCpp ------ "
-
-        if [ -z ${ACPP_VERSION+x} ]
-        then
-            echo "-> git clone $ACPP_URL $ACPP_GIT_DIR"
-            git clone $ACPP_URL $ACPP_GIT_DIR || return
-        else
-            echo "-> git clone -b $ACPP_VERSION $ACPP_URL $ACPP_GIT_DIR"
-            git clone -b $ACPP_VERSION $ACPP_URL $ACPP_GIT_DIR || return
-        fi
-
-        echo " ------  AdaptiveCpp Cloned  ------ "
-
-    fi
-
-}
-
 if which ccache &>/dev/null; then
     # to debug
     #export CCACHE_DEBUG=1
