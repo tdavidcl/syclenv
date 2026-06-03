e814a7032cec29a48962df29c96ae7eb426df141
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-06-03 18:29:28 +0200

    add macos

--- src/syclenv/utils/macos.py
+++ src/syclenv/utils/macos.py
@@ -3,5 +3,5 @@ from syclenv.run_cmd import run_cmd
 
 def has_brew_packages(package_list: list[str]) -> bool:
     return run_cmd(
-        f"brew list {' '.join(package_list)} >/dev/null 2>&1", log_error=True
+        f"brew list {' '.join(package_list)} >/dev/null 2>&1", log_error=False
     )
