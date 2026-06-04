f17a6c04bef339e8c801c91a15f5775321a82963
Timothée David--Cléris <timothee.davidcleris@proton.me>
2026-06-04 23:57:35 +0200

    fix workflow

--- src/syclenv/helper_scripts/helper_script_utils.py
+++ src/syclenv/helper_scripts/helper_script_utils.py
@@ -1,17 +1,17 @@
 from importlib.resources import files
-from os.path import dirname
+from importlib.resources.abc import Traversable
 
 SPACER = "\n###########################################################################"
 
 
-def fetch_helper_script(path: str) -> str:
-    with open(path) as f:
+def fetch_helper_script(path: Traversable) -> str:
+    with path.open() as f:
         helper_script = ""
-        helper_script += f"\n{SPACER}\n# Imported script {path}{SPACER}\n"
+        helper_script += f"\n{SPACER}\n# Imported script {path.name}{SPACER}\n"
         helper_script += f.read()
         helper_script += f"{SPACER}{SPACER}{SPACER}\n\n"
 
         return helper_script
 
 
-HELPER_SCRIPTS_DIR = dirname(files(__package__))
+HELPER_SCRIPTS_DIR = files(__package__)
